9455ac37caeb81ed5967721d5fab67618632bc51
Keng <kengthao57@gmail.com>
2023-10-10 21:11:28 -0500

    Made final edit

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # daily-deja-vu
+
+## Description
+This work day schedule alllows the user to add an event to a working hour schedule (9-5pm). The different colored blocks indicates the past, present, and future hour. Past time blocks will appear gray, present time block will appear red, and future time block will appear green. 
+
+# Challenge
+
+## User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+## Acceptance Criteria
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours of 9am to 5pm
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+## Screenshots
+
+![Alt text](<Screenshot 2023-10-10 at 8.48.14 PM.png>)
+
+![Alt text](<Screenshot 2023-10-10 at 8.49.24 PM.png>)
+
+
+<../Desktop/Screen Recording 2023-10-10 at 8.55.03 PM.mov>
+
+## Usage Instructions
+
+1. Open up the browser during workday (9-5pm)
+2. Click on time block
+3. Enter in event for the day in the time block
+4. Click the save icon on the right
+
+## Source URL
+
+## Installation
+1. Clone github Repository
+2. Download the Zip
+
+## Credits
+Day.JS
+JQuery
+W3Schools
+Mozilla
+Visual Studio
+Google
+My partner in crime Bao in the same class
+
+## What I Learned
+I have learned how to incorporate Dayjs into a website while using JQuery as well. It was a fun but frustrating process. I will need to do more JQuery researches and expand my knowledge on Dayjs.
+
+## Author
+My Github account
+https://github.com/Kthao29
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-});
-
 
 //The current day
 var today = dayjs(); 
@@ -43,7 +20,7 @@ setInterval(updateTime, 1000);
 console.log(updateTime);
 
 
-
+//Button to setItems into localStorage
 $(".saveBtn").on("click", function (event) {
   event.preventDefault();
 
@@ -52,4 +29,29 @@ $(".saveBtn").on("click", function (event) {
   localStorage.setItem(time, description);
 
 });
+//Function to getItem from localStorage
+$('.time-block').each(function() {
+  const myTime = $(this).attr('id');
+  const myDescription = localStorage.getItem(myTime);
+  $(this).children('.description').val(myDescription);
+});
 
+//Function to get the past,future, and present time block
+function getColor() {
+  $('.time-block').each(function() {
+    const currentHour = dayjs().format('H');
+    const blockHour = parseInt(this.id);
+    if (blockHour == currentHour) {
+      $(this).addClass('present');
+    } else if (blockHour < currentHour) {
+      $(this).addClass('past');
+    } else {
+      $(this).addClass('future');
+    }
+  });
+}
+
+
+//Call the function
+getColor();
+updateTime();
